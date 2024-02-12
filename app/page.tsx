d1af982ca927e4a2e1be35a45e39d6fb0eb99021
Cristhian Almeida <3correntes@gmail.com>
2024-02-11 22:01:37 -0400

    add: mostrar mais

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components';
 import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars } from '@/utils';
 
@@ -40,6 +40,11 @@ export default async function Home({ searchParams }) {
                 <CarCard car={car} key={car.id} />
               ))}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
--- components/index.ts
+++ components/index.ts
@@ -6,6 +6,18 @@ import SearchManufacturer from './SearchManufacturer';
 import CustomFilter from './CustomFilter';
 import CarCard from './CarCard';
 import CarDetails from './CarDetails';
+import ShowMore from './ShowMore';
 import Footer from './Footer';
 
-export { Navbar, Hero, CustomButton, SearchBar, SearchManufacturer, CustomFilter, CarCard, CarDetails, Footer };
+export {
+  Navbar,
+  Hero,
+  CustomButton,
+  SearchBar,
+  SearchManufacturer,
+  CustomFilter,
+  CarCard,
+  CarDetails,
+  ShowMore,
+  Footer
+};
