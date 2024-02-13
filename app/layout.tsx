d4a85cbd6e080289dff51c1f82ad2bb08e2c075a
Cristhian Almeida <3correntes@gmail.com>
2024-02-13 17:03:48 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,27 @@
-import './globals.css';
+// app/layout.tsx
 
+import { ReactNode } from 'react';
+import Head from 'next/head';
 import { Footer, Navbar } from '@/components';
+
+interface LayoutProps {
+  children: ReactNode;
+}
 
 export const metadata = {
   title: 'Vitriny - Aluguel de Carros',
   description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.',
-  og: {
-    title: 'Vitriny - Aluguel de Carros',
-    description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.',
-    url: 'https://www.vitriny.vercel.app/',
-    siteName: 'Vitriny',
-    images: [
-      {
-        url: 'https://i.ibb.co/CnPhpKM/vitriny.png',
-        width: 1200,
-        height: 630
-      }
-    ],
-    type: 'website',
-    locale: 'pt_BR'
-  },
+  image: 'https://i.ibb.co/CnPhpKM/vitriny.png'
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Head>
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+      </Head>
       <body className="relative">
         <Navbar />
         {children}
