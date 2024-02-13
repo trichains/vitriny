9cd4f614e7636b778efdaf9f0932109743c764dc
Cristhian Almeida <3correntes@gmail.com>
2024-02-13 15:32:07 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <Head>
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
+        <meta property="og:title" content="Vitriny" />
+        <meta
+          property="og:description"
+          content="Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem."
+        />
+        <meta property="og:image" content="https://i.ibb.co/CnPhpKM/vitriny.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://vitriny.vercel.app" />
       </Head>
       <body className="relative">
         <Navbar />
