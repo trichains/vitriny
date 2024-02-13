78627a53925207e87d78248fa26f1335949ed3b3
Cristhian Almeida <3correntes@gmail.com>
2024-02-13 17:05:55 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,15 @@
-// app/layout.tsx
+import './globals.css';
 
-import { ReactNode } from 'react';
-import Head from 'next/head';
 import { Footer, Navbar } from '@/components';
-
-interface LayoutProps {
-  children: ReactNode;
-}
 
 export const metadata = {
   title: 'Vitriny - Aluguel de Carros',
-  description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.',
-  image: 'https://i.ibb.co/CnPhpKM/vitriny.png'
+  description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.'
 };
 
-export default function Layout({ children }: LayoutProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Head>
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-      </Head>
       <body className="relative">
         <Navbar />
         {children}
