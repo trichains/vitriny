de8a872bf86d7fe7d20552dcd3de735893512e2e
Cristhian Almeida <3correntes@gmail.com>
2024-02-13 16:54:39 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,22 @@ import { Footer, Navbar } from '@/components';
 
 export const metadata = {
   title: 'Vitriny - Aluguel de Carros',
-  description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.'
+  description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.',
+  og: {
+    title: 'Vitriny - Aluguel de Carros',
+    description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.',
+    url: 'https://www.vitriny.vercel.app/',
+    siteName: 'Vitriny',
+    images: [
+      {
+        url: 'https://i.ibb.co/CnPhpKM/vitriny.png',
+        width: 1200,
+        height: 630
+      }
+    ],
+    type: 'website',
+    locale: 'pt_BR'
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
