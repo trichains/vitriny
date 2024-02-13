631c770ef1d5ad583a7368d94fdaadb9a1841edc
Cristhian Almeida <3correntes@gmail.com>
2024-02-13 16:25:09 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,6 @@ import Head from 'next/head';
 
 import { Footer, Navbar } from '@/components';
 
-export const metadata = {
-  title: 'Vitriny - Aluguel de Carros',
-  description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.',
-  image: 'https://i.ibb.co/CnPhpKM/vitriny.png'
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
