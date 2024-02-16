61161a675ec09c5b8df060899aa1ba8250481065
Cristhian Almeida <3correntes@gmail.com>
2024-02-16 16:53:26 -0400

    add: opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,27 @@
+import type { Metadata } from 'next';
+
 import './globals.css';
 
 import { Footer, Navbar } from '@/components';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Vitriny - Aluguel de Carros',
-  description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.'
+  description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.',
+  openGraph: {
+    title: 'Vitriny - Aluguel de Carros',
+    description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.',
+    url: 'https://vitriny.vercel.app/',
+    siteName: 'Vitriny - Aluguel de Carros',
+    images: [
+      {
+        url: 'https://i.ibb.co/CnPhpKM/vitriny.png',
+        width: 1200,
+        height: 630
+      }
+    ],
+    locale: 'pt-BR',
+    type: 'website'
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
