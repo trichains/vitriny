ebe224ef9bc33e8cc617a259822c5d7f1614e2c9
Cristhian Almeida <3correntes@gmail.com>
2024-02-16 17:39:54 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,14 @@
-import type { Metadata } from 'next';
-import Head from 'next/head';
 import { Footer, Navbar } from '@/components';
 
 import './globals.css';
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Vitriny - Aluguel de Carros',
   description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.',
+  url: 'https://vitriny.vercel.app/',
   openGraph: {
     title: 'Vitriny - Aluguel de Carros',
     description: 'Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem.',
-    url: 'https://vitriny.vercel.app/',
     siteName: 'Vitriny - Aluguel de Carros',
     images: [
       {
@@ -20,28 +18,14 @@ export const metadata: Metadata = {
       }
     ],
     locale: 'pt-BR',
-    type: 'website'
+    type: 'website',
+    url: 'https://vitriny.vercel.app/'
   }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Head>
-        <title>Vitriny - Aluguel de Carros</title>
-        <meta property="og:title" content="Vitriny - Aluguel de Carros" />
-        <meta
-          name="description"
-          content="Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem."
-        />
-        <meta
-          property="og:description"
-          content="Encontre os melhores carros para alugar e tenha a experiência perfeita em sua viagem."
-        />
-        <meta property="og:url" content="https://vitriny.vercel.app/" />
-        <meta property="og:image" content="https://i.ibb.co/CnPhpKM/vitriny.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <body className="relative">
         <Navbar />
         {children}
